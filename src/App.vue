a4a46f259f9b3a2718d1deb031869c0d9a4d452c
alex-sozdat-sajt <schetchik2016@yandex.ru>
2021-03-18 20:07:50 +0300

    6 ch

--- src/App.vue
+++ src/App.vue
@@ -201,8 +201,31 @@ export default {
   created: function() {
     // console.log("addCoin");
     this.addCoin();
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+    }
+    this.subscribeToUpdates();
+    this.tickers.forEach(ticker=>{
+       this.subscribeToUpdates(ticker.name);
+    })
   },
   methods: {
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=7310e32e5cce03db8aa2a8f947ef99f69844cf38cc168835c16aff4964c17d12`
+        );
+        const data = await f.json();
+
+        this.tickers.find(t => t.name === tickerName).price =
+          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        if (this.sel?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      }, 3000);
+      this.ticker = "";
+    },
     clicToCoin(s) {
       this.ticker = s;
       this.add();
@@ -271,19 +294,15 @@ export default {
         this.exist = true;
       } else {
         this.tickers.push(currentTicker);
-        setInterval(async () => {
-          const f = await fetch(
-            `https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=7310e32e5cce03db8aa2a8f947ef99f69844cf38cc168835c16aff4964c17d12`
-          );
-          const data = await f.json();
+        console.log("tickers", this.tickers);
+        localStorage.setItem(
+          "cryptonomicon-list",
+          JSON.stringify(this.tickers)
+        );
+        //!!!!!!!! делал без JSON.stringify - не работает!!!!!!!!
+        this.subscribeToUpdates(currentTicker.name);
 
-          this.tickers.find(t => t.name === currentTicker.name).price =
-            data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-          if (this.sel?.name === currentTicker.name) {
-            this.graph.push(data.USD);
-          }
-        }, 3000);
-        this.ticker = "";
+         
       }
     },
     select(ticker) {
